cdd8d1ddc1fae6cd441309f51a3c990ee0c22da5
Mohamed Kaleel <145964512+mohdkaleel@users.noreply.github.com>
2025-12-09 09:11:33 +0530

    Add files via upload

---
+++ StudentManagementWebApplication.java
@@ -0,0 +1,13 @@
+package com.studentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementWebApplication.class, args);
+	}
+
+}
